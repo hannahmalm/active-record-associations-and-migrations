7ac7e9961f9d453114b9a11d83eb24ab69502c45
Hannah Malm <hannahemalm616@gmail.com>
2021-06-26 16:08:46 -0400

    commit tree 3b397c885907955ceac0ff68fc774dc58a6c314c

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,2 +1,8 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
-end
+    def change #create an artists table that only has a name
+      create_table :artists do |t| 
+        t.string :name 
+      end 
+    end
+  end
+  
--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,10 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
-end
+    def change
+     create_table :songs do |t| #create a songs table that has a name, artist_id and genre validates_each
+       t.string :name
+       t.integer :artist_id
+       t.integer :genre_id
+     end 
+   end
+ end
+ 
